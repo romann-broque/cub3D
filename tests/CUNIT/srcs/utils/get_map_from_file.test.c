4bbfe74d702c5c5399391d9d49465f99b87462d4
romann-broque <kinoide56@gmail.com>
2023-10-10 08:29:59 +0000

    adapt cunit tests to new format of get_file return

--- tests/CUNIT/srcs/utils/get_map_from_file.test.c
+++ tests/CUNIT/srcs/utils/get_map_from_file.test.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 20:55:42 by rbroque           #+#    #+#             */
-/*   Updated: 2023/10/09 20:56:03 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/10/10 08:29:19 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 t_map	*get_map_from_file(const char *const filename)
 {
-	char *const	content = get_file(filename);
-	t_map		*map;
+	char **const	content = get_file(filename);
+	t_map			*map;
 
 	if (content == NULL)
 		return (NULL);
