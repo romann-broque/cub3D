bd23a2accc024c4c6867f84e8e993333e89723e1
romann-broque <kinoide56@gmail.com>
2023-10-11 07:18:36 +0000

    fix is_unique_direction function

--- srcs/map/is_map_valid/is_map_content_valid.c
+++ srcs/map/is_map_valid/is_map_content_valid.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   is_map_content_valid.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jess <jess@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 13:33:53 by jrouillo          #+#    #+#             */
-/*   Updated: 2023/10/10 16:40:18 by jess             ###   ########.fr       */
+/*   Updated: 2023/10/11 07:18:13 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,13 @@ static bool	is_unique_direction(const t_map *const map)
 
 	count = 0;
 	i = 0;
-	j = 0;
 	while (i < map->height)
 	{
 		j = 0;
 		while (j < map->width)
 		{
-			count += count_c_in_str(&map->matrix[i][j].tile_char,
-					VALID_CHAR_DIR[i]);
+			count += count_c_in_str(VALID_CHAR_DIR,
+					map->matrix[i][j].tile_char);
 			j++;
 		}
 		i++;
@@ -41,7 +40,6 @@ static bool	is_charset_valid(const t_map *const map)
 	size_t	j;
 
 	i = 0;
-	j = 0;
 	while (i < map->height)
 	{
 		j = 0;
