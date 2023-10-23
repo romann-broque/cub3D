d598947bfbd9b9733dd65835429f7dc7c51b152c
romann-broque <kinoide56@gmail.com>
2023-10-23 06:03:13 +0000

    clean init_player

--- srcs/window/init_window.c
+++ srcs/window/init_window.c
@@ -6,11 +6,16 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 09:39:32 by rbroque           #+#    #+#             */
-/*   Updated: 2023/10/19 06:54:25 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/10/23 06:01:42 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static void	init_player(t_map *const map)
+{
+	map->player = get_player(map);
+}
 
 void	init_window(
 	t_win *const window,
@@ -32,6 +37,7 @@ void	init_window(
 		return ;
 	}
 	init_data(window->mlx_ptr, &window->data);
+	init_player(map);
 	window->map = map;
 	window->config = config;
 }
