15827ae06f8bc48c61a881680e6331fbf2f60ee7
romann-broque <kinoide56@gmail.com>
2023-10-28 22:57:04 +0000

    change refresh function

--- srcs/window/display/refresh.c
+++ srcs/window/display/refresh.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 08:09:03 by rbroque           #+#    #+#             */
-/*   Updated: 2023/10/24 23:35:08 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/10/28 22:56:45 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 
 void	refresh(t_win *window)
 {
-	mlx_destroy_image(window->mlx_ptr, window->data.img);
-	init_data(window->mlx_ptr, &window->data);
+	mlx_clear_window(window->mlx_ptr, window->win_ptr);
 	display_window_content(window);
 	mlx_put_image_to_window(window->mlx_ptr, window->win_ptr,
 		window->data.img, 0, 0);
